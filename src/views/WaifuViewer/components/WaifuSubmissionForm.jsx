5dd44dae24feb6d5212ab5e67b65824f99d22bcb
Watson Tungjunyatham <majestic.mudkip@gmail.com>
2018-07-03 16:31:50 -0700

    Add dropzone to create drag-and-drop upload of images

--- src/views/WaifuViewer/components/WaifuSubmissionForm.jsx
+++ src/views/WaifuViewer/components/WaifuSubmissionForm.jsx
@@ -1,26 +1,55 @@
 import React from 'react';
 import { Form, Rating } from 'semantic-ui-react';
+import Dropzone from 'react-dropzone';
 import { createOptions } from "../../../lib/util";
 import withModal from "../../../hocs/withModal";
 
 const bloodTypes = ['A', 'O', 'B', 'AB', '?'];
 
 class WaifuSubmissionForm extends React.Component {
+
+  state = {
+    toSubmit: {},
+    files: {},
+  };
+
+  handleChange = (e, { name, value }) => this.setState({
+    toSubmit: {
+      ...this.state.toSubmit,
+      [name]: value,
+    }
+  });
+
+  onDrop = (file, key) => this.setState({
+    files: {
+      ...this.state.files,
+      [key]: file,
+    },
+  });
+
+  onDropThumbnail = (files) => this.onDrop(files[0], 'thumbnail');
+
+  onDropImage = (files) => this.onDrop(files[0], 'image');
+
   render() {
     const { ...rest } = this.props;
     return (
       <Form {...rest}>
         <Form.Group widths="equal">
-          <Form.Input fluid required label="Name" />
-          <Form.Select fluid required label="Blood Type" options={createOptions(bloodTypes)} defaultValue="?" />
+          <Form.Input fluid required label="Name" name="name" onChange={this.handleChange} />
+          <Form.Select fluid required label="Blood Type" name="blood_type" options={createOptions(bloodTypes)} defaultValue="?" onChange={this.handleChange} />
         </Form.Group>
         <Form.Group widths="equal">
-          <Form.Button fluid label="Thumbnail">Upload...</Form.Button>
-          <Form.Button fluid label="Main Image">Upload...</Form.Button>
+          <Dropzone name="thumbnail" multiple={false} onDrop={this.onDropThumbnail}>
+            Upload your thumbnail here...
+          </Dropzone>
+          <Dropzone name="image" multiple={false} onDrop={this.onDropImage}>
+            Upload your image here...
+          </Dropzone>
         </Form.Group>
         <label>Rating</label>
-        <Rating maxRating={5} defaultRating={0} />
-        <Form.TextArea label="Description" placeholder="Why should this girl be in the thicc hall of fame?" />
+        <Rating maxRating={5} defaultRating={0} name="rating" onChange={this.handleChange} />
+        <Form.TextArea label="Description" placeholder="Why should this girl be in the thicc hall of fame?" name="description" onChange={this.handleChange} />
         <Form.Button type="submit">Submit!</Form.Button>
       </Form>
     )
